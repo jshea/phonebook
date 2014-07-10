4731e757c2ef6b01b4d563c1c43dbda8dc23c3af
jshea <jimtshea@gmail.com>
2014-07-10 15:37:46 -0700

    prep for finishing moving $http calls to services.

--- app/js/services.js
+++ app/js/services.js
@@ -14,43 +14,11 @@ angular.module("phonebook").factory("DataFactory", function ($http, toaster) {
       }
    };
 //      ,
-//      getContactById: function (id) {
-//         var contactRef = new Firebase(FBURL + id);
-//         return $firebase(contactRef);
-//      },
-//      removeContactById: function (id) {
-//         var contactRef = new Firebase(FBURL + id);
-//         contactRef.remove();
-//         $location.path("/");
-//      },
-//      updateNameById: function (id, first, last) {
-//         var contactRef = new Firebase(FBURL + id);
-//         contactRef.update({firstname: first, lastname: last});
-//         contactRef.setPriority(last.toLowerCase() + " " + first.toLowerCase());
-//         $location.path("/view/" + id);
-//      },
-//      saveNewName: function (first, last) {
-//         var contactsRef = new Firebase(FBURL);
-//         var newContactRef = contactsRef.push();
-//
-//         newContactRef.setWithPriority({firstname: first, lastname: last}, last.toLowerCase() + " " + first.toLowerCase());
-//         $location.path("/view/" + newContactRef.name());
-//      },
-//      initializeData: function (data) {
-//         var contactsRef = new Firebase(FBURL);
-//         contactsRef.remove();
-//
-//         // Loop through the array of data inserting records individually. This
-//         // lets us set the priority for each record. Priority determines the
-//         // returned sort order.
-//         data.forEach(function (element, index, array) {
-//            var newContactRef = contactsRef.push(),
-//               first = element.firstname,
-//               last = element.lastname;
-//            newContactRef.setWithPriority({firstname: first, lastname: last}, last.toLowerCase() + " " + first.toLowerCase());
-//         });
-//         $location.path("/");
-//      }
+//      getContact:     function (id) { },
+//      removeContact:  function (id) { },
+//      updateContact:  function (id, object) { },
+//      addContact:     function (obj) { },
+//      initializeData: function (data) { }
 
 });
 
